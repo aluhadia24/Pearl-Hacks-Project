509204c716a74f8e4cdae753f205df4629554587
Ashna_Luhadia <aluhadia24@gmail.com>
2025-02-15 18:08:08 -0500

    button change

--- website/main.js
+++ website/main.js
@@ -1,19 +1,23 @@
 var two_hundred = document.getElementById("200")
 two_hundred.onclick = function(){
-	two_hundred.style.backgroundColor = "blue"; 
+	two_hundred.style.backgroundColor = "lightblue"; 
 	var meal_plan = 200
 }
 var one_sixty = document.getElementById("160")
 one_sixty.onclick = function(){ 
+	one_sixty.style.backgroundColor = "lightblue"; 
 	var meal_plan = 160
 }
 var one_twenty = document.getElementById("120")
 one_twenty.onclick = function(){ 
+	one_twenty.style.backgroundColor = "lightblue"; 
 	var meal_plan = 120
 }
 var one_hundred = document.getElementById("100")
 one_hundred.onclick = function(){ 
+	one_hundred.style.backgroundColor = "lightblue"; 
 	var meal_plan = 100
+	//test
 }
 
 
