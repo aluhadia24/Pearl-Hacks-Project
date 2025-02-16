41dbb9f2296a9d1d688fadcf403dbde504d71f54
amzheng06 <amzheng@unc.edu>
2025-02-16 08:47:34 -0500

    changed styles name

--- website/main.js
+++
@@ -1,9 +0,0 @@
-var two_hundred = document.getElementById("200")
-var pig_emoji = "🐷"
-two_hundred.onclick = function(){
-	if(!two_hundred.innerHTML.includes(pig_emoji)){
-		two_hundred.innerHTML = pig.innerHTML + pig_emoji
-	} else {
-		pig.innerHTML = "pig"
-	}
-}
