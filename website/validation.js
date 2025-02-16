7f6584f6cd9ddd561c04f8f0ded5a447488be183
enmendez <enmendez@unc.edu>
2025-02-15 22:49:32 -0500

    login page

--- website/validation.js
+++ website/validation.js
@@ -1,4 +1,18 @@
 const form = document.getElementById('form')
 const username_input = document.getElementById('username-input')
-const password_input = document.getElementById('passwrd-input')
+const password_input = document.getElementById('password-input')
 
+
+function getLoginFormErrors(username, password){
+    let errors = []
+}
+if(username == '' || username == null){
+    errors.push('Username is required')
+   firstname_input.parentElement.classList.add('incorrect') 
+}
+if(password == '' || password == null){
+    errors.push('Password is required')
+   password_input.parentElement.classList.add('incorrect') 
+}
+
+return errors;
